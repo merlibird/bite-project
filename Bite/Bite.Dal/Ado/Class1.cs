bc94e7244fc1e6fd76900e1004b7b244ce677c1b
sanin-zc <saninzimic@gmail.com>
2026-04-28 21:29:10 +0200

    add folder structure

---
+++ Bite/Bite.Dal/Ado/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bite.Dal.Ado
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Bite/Bite.Dal/Interface/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bite.Dal.Interface
+{
+    internal class Class1
+    {
+    }
+}
