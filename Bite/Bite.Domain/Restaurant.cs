072417be45df66eee8eca35bbbdca04fbc7cae27
Tarik <179576875+merlibird@users.noreply.github.com>
2026-05-14 14:36:01 +0200

    Adapt Restaurant Domain

--- Bite/Bite.Domain/Restaurant.cs
+++ Bite/Bite.Domain/Restaurant.cs
@@ -9,25 +9,23 @@ namespace Bite.Domain
         string name,
         int menuId,
         int addressId,
-        string webhookUrl,
-        string titleImagePath)
+        string? webhookUrl,
+        string? titleImagePath,
+        DateTime createdAt,
+        DateTime updatedAt)
     {
-        public int Id { get; set; } = id;
+        public int Id { get; init; } = id;
         public string Name { get; set; } = name;
         public int MenuId { get; set; } = menuId;
         public int AddressId { get; set; } = addressId;
-        public string WebhookUrl { get; set; } = webhookUrl;
-        public string TitleImagePath { get; set; } = titleImagePath;
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-
-        // Navigation (optional, wird nicht immer befüllt)
-        public Address? Address { get; set; }
-        public List<OpeningHourSlot> OpeningHours { get; set; } = new();
+        public string? WebhookUrl { get; set; } = webhookUrl;
+        public string? TitleImagePath { get; set; } = titleImagePath;
+        public DateTime CreatedAt { get; init; } = createdAt;
+        public DateTime UpdatedAt { get; set; } = updatedAt;
 
         public override string? ToString()
         {
-            return $"Restaurant {Id}: {Name} (MenuId: {MenuId}, AddressId: {AddressId})";
+            return $"Restaurant {Id}: {Name} (MenuId: {MenuId}, AddressId: {AddressId}), CreatedAt: {CreatedAt}, UpdatedAt: {UpdatedAt}";
         }
     }
 }
