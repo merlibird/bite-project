4d2b7191e0e3e35cca023f2ba03e0862cc1a7cd8
Tarik <179576875+merlibird@users.noreply.github.com>
2026-05-18 22:29:04 +0200

    add base structure for Dao Integration tests

---
+++ Bite/Bite.Tests/IntegrationTests/DatabaseFixture.cs
@@ -0,0 +1,22 @@
+﻿using Bite.Dal.Ado;
+using Bite.Dal.Common;
+using Bite.Dal.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bite.Tests.IntegrationTests;
+
+public class DatabaseFixture : IAsyncLifetime
+{
+    public IConnectionFactory ConnectionFactory { get; private set; } = null!;
+
+    public async Task InitializeAsync() // beforeAll --> initialize database connection
+    {
+        var configuration = ConfigurationUtil.GetConfiguration();
+        ConnectionFactory = DefaultConnectionFactory.FromConfiguration(configuration, "BiteDbConnection", "ProviderName");
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask; // afterAll --> do nothing
+}
---
+++ Bite/Bite.Tests/IntegrationTests/RestaurantDaoTests.cs
@@ -0,0 +1,32 @@
+﻿using Bite.Dal.Ado;
+using Bite.Dal.Common;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bite.Tests.IntegrationTests;
+
+public class RestaurantDaoTests : IClassFixture<DatabaseFixture>, IAsyncLifetime
+{
+    private readonly RestaurantDao dao;
+    private readonly AdoTemplate template;
+
+    public RestaurantDaoTests(DatabaseFixture fixture)
+    {
+        dao = new RestaurantDao(fixture.ConnectionFactory);
+        template = new AdoTemplate(fixture.ConnectionFactory);
+    }
+
+    public async Task InitializeAsync() // beforeEach --> clear the Restaurant table
+        => await template.ExecuteAsync("delete from Restaurant");
+
+    public Task DisposeAsync() => Task.CompletedTask; // afterEach --> do nothing
+
+    [Fact]
+    public async Task FindAllAsync_EmptyTable_ReturnsEmptyList() 
+    {
+        var result = await dao.FindAllAsync();
+        Assert.Empty(result);
+    }
+}
