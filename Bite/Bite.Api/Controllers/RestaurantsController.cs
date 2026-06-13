292ff6375c73fba107106dfbaed406bf61fa4c76
Sanin Zimic <saninzimic@gmail.com>
2026-06-13 15:35:30 +0200

    Feature/issue 47 implement put menu (#58)
    * Add implementation for EP
    
    * minor changes / fixes
    
    * Delete double RestaurantController
    
    * add the correct controller

--- Bite/Bite.Api/Controllers/RestaurantsController.cs
+++ Bite/Bite.Api/Controllers/RestaurantsController.cs
@@ -1,18 +1,38 @@
 using Bite.Api.Dtos;
+using Bite.Api.Dtos.Mappers;
 using Bite.Domain;
 using Bite.Services.Common;
 using Bite.Services.Interface;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace Bite.Api.Controllers
 {
     [ApiController]
-    [Route("api/restaurants")]
-    public class RestaurantController(IRestaurantService restaurantService, 
+    [Route("api/[controller]")]
+    public class RestaurantsController(IRestaurantService restaurantService, 
         IWebHostEnvironment env) : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<RestaurantSearchResultDto>> SearchRestaurants(
+        [FromQuery, Range(-90, 90)] double latitude,
+        [FromQuery, Range(-180, 180)] double longitude,
+        [FromQuery] bool openNow = false,
+        [FromQuery, Range(1, 100)] int count = 10,
+        CancellationToken cancellationToken = default)
+        {
+            var restaurants = await restaurantService.SearchRestaurantsAsync(
+                latitude,
+                longitude,
+                openNow,
+                count,
+                cancellationToken);
+
+            return Ok(restaurants.ToRestaurantSearchResultDto(latitude, longitude, openNow, count));
+        }
+
         [HttpPost]
         [Consumes("multipart/form-data")]
         public async Task<IActionResult> Register([FromForm] RegisterRestaurantRequest request, CancellationToken cancellationToken)
